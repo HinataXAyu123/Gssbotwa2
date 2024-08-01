644c53390a756cb37d3015fbf4ab8f0e8acab8ad
HinataXAyu123 <nbobita76@gmail.com>
2024-08-01 16:34:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,20 +7,20 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
-global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.ownername = "𝞢𝙎𝙏𝞢𝙇𝙇𝞓✨🍷";
+global.owner = ['918473891245'];
+global.premium = ['918473891245'];
+global.packname = "𝞢𝙎𝙏𝞢𝙇𝙇𝞓✨🍷";
+global.author = "𝞢𝙎𝙏𝞢𝙇𝙇𝞓✨🍷";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
 global.menuType = "2"
 global.autoswview = true
 global.autoread = false
-global.available = false
-global.autoTyping = false
-global.autoRecord = false
+global.available = true
+global.autoTyping = true
+global.autoRecord = true 
 global.autoreact = false
 global.onlygroup = false
 global.onlypc = false
@@ -32,8 +32,8 @@ global.mess = {
     banned: '`You are *Banned* fron using commands!`',
     bangc: '`This Group is *Banned* from using Commands!`',
     success: '`✓ Success`',
-    admin: '`This feature is only for group admins`',
-    botAdmin: '`I am not an admin!`',
+    admin: '`bitch This feature is only for group admins`',
+    botAdmin: '`idiot gimme admin!`',
     owner: '`You are not my owner`',
     group: '`You can use this command only in groups ❌`',
     private: '`Feature is used only for private chats!`',
